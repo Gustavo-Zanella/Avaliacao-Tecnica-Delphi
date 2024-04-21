facfabfe249edf7a9251c6c5789a1f5208d0ec8e
Gustavo-Zanella <gustavozanella04@gmail.com>
2024-04-21 08:26:23 -0300

    Ajuste DM

---
+++ Delphi/__recovery/unDataModule.pas
@@ -0,0 +1,33 @@
+unit unDataModule;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.PG,
+  FireDAC.Phys.PGDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  FireDAC.Comp.DataSet, Vcl.Dialogs;
+
+type
+  TDM = class(TDataModule)
+    conDB: TFDConnection;
+    qryGeral: TFDQuery;
+    PgDriverLink: TFDPhysPgDriverLink;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DM: TDM;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
